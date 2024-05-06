2e152d69a773265f5762217c2ddb2119f4793a66
bora <bora44144@gmail.com>
2024-05-07 00:38:31 +0900

    [Feat] 싱글톤

---
+++ app/src/main/java/com/standard/multiviewtyperecyclerview/data/DataSource.kt
@@ -0,0 +1,18 @@
+package com.standard.multiviewtyperecyclerview.data
+
+class DataSource {
+    companion object {
+        private var INSTANCE: DataSource? = null
+        fun getDataSource(): DataSource {
+            return synchronized(DataSource::class) {
+                val newInstance = INSTANCE ?: DataSource()
+                INSTANCE = newInstance
+                newInstance
+            }
+        }
+    }
+
+    fun getCardList(): List<Card> {
+        return cardList()
+    }
+}
