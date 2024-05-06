6d01ad7741f2687cf74ce43a6c9853d31ceae145
bora <bora44144@gmail.com>
2024-05-07 00:38:21 +0900

    [Feat] Parcelable

--- app/src/main/java/com/standard/multiviewtyperecyclerview/data/Card.kt
+++ app/src/main/java/com/standard/multiviewtyperecyclerview/data/Card.kt
@@ -1,3 +1,12 @@
 package com.standard.multiviewtyperecyclerview.data
 
-data class Card(val name: String, val number: String, val period: String, val balance: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Card(
+    val name: String,
+    val number: String,
+    val period: String,
+    val balance: String
+) : Parcelable
